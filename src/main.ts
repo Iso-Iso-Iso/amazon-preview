51de43ea7f22fa71b1cbc22ef1d074c6924ce225
Oleg-Ivnv <oleg.ivanov@waites.net>
2024-01-15 22:07:05 +0200

    git

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as process from "process";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    app.enableCors()
     await app.listen(+process.env.PORT);
 }
 bootstrap();
